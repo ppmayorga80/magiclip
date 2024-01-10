169565cb68221480eda5bcfee486326ed733b8f2
Pedro Mayorga <ppmayorga80@gmail.com>
2024-01-10 14:01:10 -0600

    first functional version

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ ! -d venv ]; then
+  echo "Creating venv"
+  python3.10 -m venv venv
+fi
+
+source venv/bin/activate
+
+if [[ ! $PYTHONPATH =~ $PWD ]]; then
+  echo "Exporting PYTHONPATH"
+  export PYTHONPATH="$PWD:$PWD/src:$PYTHONPATH"
+fi
+
+pip install --upgrade pip
+pip install pipenv
+pip install -r requirements.txt
+
+#extra packages to install
+pip install -r requirements-local.txt
